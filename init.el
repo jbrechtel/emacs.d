239adfa3feeac1ef5c2dd47282e1f1672b8a322f
James Brechtel <jbrechtel@gmail.com>
2013-09-16 21:24:43 -0400

    personalizing emacs config...

--- init.el
+++ init.el
@@ -1,42 +1,80 @@
+(global-font-lock-mode t)
 
-(setq load-path
-      (append
-       (mapcar
-        'expand-file-name
-        '(
-          "~/.emacs.d/site-lisp/cedet/common"
-          "~/.emacs.d/site-lisp/ecb"
-          "~/.emacs.d/site-lisp/elib"
-          "~/.emacs.d/site-lisp/emacs-rails"
-          "~/.emacs.d/site-lisp/erc"
-          "~/.emacs.d/site-lisp/fsharp"
-          "~/.emacs.d/site-lisp/fuel"
-          "~/.emacs.d/site-lisp/jde/lisp"
-          "~/.emacs.d/site-lisp/mmm"
-          "~/.emacs.d/site-lisp/mozart"
-          "~/.emacs.d/site-lisp/nxml"
-          "~/.emacs.d/site-lisp/ocaml"
-          "~/.emacs.d/site-lisp/sml"
-          "~/.emacs.d/site-lisp/own"
-          "~/.emacs.d/site-lisp"
-          "~/.emacs.d/"
-          )) load-path))
+(windmove-default-keybindings)
 
-(setq custom-file (expand-file-name "~/.emacs.d/emacs-custom.el"))
+(setq use-file-dialog nil)
 
-(require 'emacs-custom)
-(require 'emacs-require)
-(require 'emacs-vars)
-(require 'emacs-automode)
-(require 'emacs-jde)
-(require 'emacs-functions)
-(require 'emacs-hooks)
-(require 'emacs-keys)
-(require 'emacs-mac)
-(require 'emacs-utf8)
-(require 'emacs-short)
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
 
-;; This file contains user specific customization and should not be checked in to the shared repository
-;; No error will be displayed if it can't be loaded
-(load "emacs-user-init" t)
-(put 'downcase-region 'disabled nil)
+;; Highlight regions and add special behaviors to regions.
+;; "C-h d transient" for more info
+(setq transient-mark-mode t)
+
+;; Display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+(global-set-key "\C-c\C-w" 'backward-kill-word)
+
+;; Remove unnecessary gui stuff
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+
+(blink-cursor-mode 0) ;; no blink
+
+(set-background-color "black")
+(set-face-background 'default "black")
+(set-face-background 'region "black")
+(set-face-foreground 'default "white")
+(set-face-foreground 'region "gray60")
+(set-face-attribute  'default nil :height 130)
+(set-foreground-color "white")
+(set-cursor-color "red")
+
+;;semantic-ectag-util.el
+
+(defcustom semantic-ectag-program "/usr/bin/ctags-exuberant"
+  "The Exuberent CTags program to use."
+  :group 'semantic
+  :type 'program)
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+(ido-mode)
+(evil-mode 1)
+
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" default)))
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(vc-annotate-background "#93a1a1")
+ '(vc-annotate-color-map (quote ((20 . "#990A1B") (40 . "#FF6E64") (60 . "#cb4b16") (80 . "#7B6000") (100 . "#b58900") (120 . "#DEB542") (140 . "#546E00") (160 . "#859900") (180 . "#B4C342") (200 . "#3F4D91") (220 . "#6c71c4") (240 . "#9EA0E5") (260 . "#2aa198") (280 . "#69CABF") (300 . "#00629D") (320 . "#268bd2") (340 . "#69B7F0") (360 . "#d33682"))))
+ '(vc-annotate-very-old-color "#93115C"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'solarized-light)
